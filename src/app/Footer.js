d255a762f6c51f2dbaa91937df254263be5cb0af
Diptangshu Chakrabarty <cdiptangshu@gmail.com>
2023-10-09 23:51:54 +0530

    add cc license in footer

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -1,12 +1,52 @@
 import React from 'react';
+import './Footer.css';
 
 export default function Footer() {
   return (
-    <div className="p-3 text-right text-xs">
-      <div>
-        Created by <a href="mailto:cdiptangshu@gmail.com">Diptangshu Chakrabarty</a>
-      </div>
-      <div>(License information)</div>
+    <div className="text-right text-xs">
+      <p
+        {...{
+          'xmlns:cc': 'http://creativecommons.org/ns#',
+          'xmlns:dct': 'http://purl.org/dc/terms/'
+        }}
+      >
+        <a
+          rel="cc:attributionURL"
+          href="https://github.com/cdiptangshu/stomp-client"
+          {...{ property: 'dct:title' }}
+        >
+          Stomp Client
+        </a>{' '}
+        by{' '}
+        <a
+          rel="cc:attributionURL dct:creator"
+          href="https://github.com/cdiptangshu"
+          {...{ property: 'cc:attributionName' }}
+        >
+          Diptangshu Chakrabarty
+        </a>{' '}
+        is licensed under{' '}
+        <a
+          href="http://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1"
+          target="_blank"
+          rel="license noopener noreferrer"
+          style={{ display: 'inline-block' }}
+        >
+          CC BY-SA 4.0
+          <img
+            style={{ height: '22px', marginLeft: '3px', verticalAlign: 'text-bottom' }}
+            src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"
+          />
+          <img
+            style={{ height: '22px', marginLeft: '3px', verticalAlign: 'text-bottom' }}
+            src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1"
+          />
+          <img
+            style={{ height: '22px', marginLeft: '3px', verticalAlign: 'text-bottom' }}
+            src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1"
+          />
+        </a>
+      </p>
     </div>
   );
 }
