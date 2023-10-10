f748552a772e8723c286c9d8a176a9246dad6649
Diptangshu Chakrabarty <cdiptangshu@gmail.com>
2023-10-10 19:57:38 +0530

    disable react dev tools on prod

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
 import Dashboard from './Dashboard';
 import Footer from './Footer';
 import Topbar from './Topbar';
@@ -8,6 +10,10 @@ import 'primereact/resources/themes/saga-green/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 export default function App() {
   return (
